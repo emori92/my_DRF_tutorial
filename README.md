db02e882bf112e3d96ddbdf58f169554d1c9f37d
falcon92 <tktk.tktk.ixjs@gmail.com>
2021-02-23 23:16:17 +0900

    views.py: create model, serializer and view

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 ## My Django RestFramework
-[現場で使えるDjango REST frameowrkの薄い本](https://booth.pm/ja/items/1314617)で登場する、SPAのチュートリアルを自分用にまとめています。
+[現場で使えるDjango REST frameowrkの薄い本](https://booth.pm/ja/items/1314617)で登場する、5章のSPAのチュートリアルを自分用にまとめています。
 
 ## environment
-Django               2.2.16
-django-debug-toolbar 3.1.1
-django-environ       0.4.5
-Pillow               8.0.1
+Django               2.2.18
---
+++ apiv1/views.py
@@ -0,0 +1,13 @@
+from shop.models import Book
+# Django REST Framewok
+from .serializers import BookSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """BookモデルのAPI`"""
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly)
